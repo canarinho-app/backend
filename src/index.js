a79945ded8b0068bb93993dfe847f39513d47192
Ionesio Junior <ionesio@lsd.ufcg.edu.br>
2019-04-22 13:53:48 -0300

    Add new routes to index source file

--- src/index.js
+++ src/index.js
@@ -3,8 +3,15 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const userRoute = require('./routes/user');
+const contentRoute = require('./routes/content')
+const commentRoute = require('./routes/comment')
+const tweetRoute = require('./routes/tweet')
+
 
 app.use(bodyParser.json());
 app.use(userRoute);
+app.use(contentRoute);
+app.use(commentRoute);
+app.use(tweetRoute);
 
-app.listen(3000, () => console.log("Server has started on port 3000."));
+app.listen(3001, () => console.log("Server has started on port 3000."));
