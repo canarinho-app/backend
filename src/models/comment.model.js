c1bb48c5e5bd163851b553c6d4354f923362323f
Ionesio Junior <ionesio@lsd.ufcg.edu.br>
2019-04-22 13:54:25 -0300

    Add id field at comment | Add route to create/get/delete comments

--- src/models/comment.model.js
+++ src/models/comment.model.js
@@ -1,7 +1,27 @@
 const mongoose = require('mongoose');
 
+const options = {
+    useNewUrlParser: true
+};
+
+const mongooseConnectionString = 'mongodb://localhost:3000/Comments';
+mongoose.connect(mongooseConnectionString, options);
+
+mongoose.connection.once('open', () => {
+    console.log('Module: Comments | Status: Connected to database.')
+}).on('error', err => {
+    console.log('Module: Comments | Status: Error connecting to database', err)
+}
+);
+
+mongoose.set('useCreateIndex', true);
+
 
 const CommentSchema = new mongoose.Schema({
+    id: {
+        type: mongoose.Schema.ObjectId,
+        auto: true
+    },
     content: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Content'
---
+++ src/routes/comment.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const CommentModel = require('../models/comment.model')
+const router = express.Router();
+
+
+router.post('/comment', (req,res) => {
+    if(!req.body){
+        return res.status(400).send('Request body is missing!');
+    }
+
+    const model = new CommentModel(req.body);
+    model.save().then(doc =>{
+        if(!doc || doc.length === 0) {
+            return res.status(500).send(doc);
+        }
+        res.status(201).send(doc);
+    }).catch(err => {
+        res.status(500).json(err);
+    });
+});
+
+
+router.get('/comment', (req,res) => {
+    if(!req.query.id){
+        return res.status(404).send("Missing parameter: id");
+    }
+
+    CommentModel.findOne({
+        id: req.query.id
+    }).then(doc => {
+        res.json(doc);
+    }).catch(err =>{
+        res.status(500).json(err);
+    });
+});
+
+
+router.delete('comment', (req,res) => {
+    if( !req.body.id){
+        return res.status(400).send("Missing parameter: id");
+    }
+
+    CommentModel.findByIdAndRemove({
+        id: req.body.id
+    }).then(doc => {
+        res.json(doc);
+    }).catch( err => {
+        res.status(500).json(err);
+    });
+});
+
+
+module.exports = router;
