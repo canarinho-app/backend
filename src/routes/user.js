03172f258aaf9bd7e78188ba1dc083c3d281ec14
Dayvson Nascimendo <dayvsonwc@gmail.com>
2019-05-08 23:17:15 -0300

    Modify user creation to handle duplicate username/email

--- src/routes/user.js
+++ src/routes/user.js
@@ -9,12 +9,22 @@ const router = express.Router();
 /**
  * POST route for User creation.
  */
-router.post('/user', (req, res) => {
+router.post('/user', async function (req, res)  {
     if (!req.body) {
         return res.status(400).send('Request Body is missing!');
     }
 
     req.body.password = bcrypt.hashSync(req.body.password, 10);
+
+    const containsUsername = await findUserByUsername(req.body.username);
+    const containsEmail = await findUserByEmail(req.body.email);
+
+    if (containsUsername) {
+        return res.status(400).json({ message: 'Username already in use.' });
+    } else if (containsEmail) {
+        return res.status(400).json({ message: 'Email already in use.' });
+    }
+
     const model = new UserModel(req.body);
     model.save().then(doc => {
         if (!doc || doc.length === 0) {
@@ -25,6 +35,30 @@ router.post('/user', (req, res) => {
         res.status(500).json(err);
     });
 });
+
+/**
+ * Verify if a user already exists with the given username.
+ * @param {String} username 
+ */
+async function findUserByUsername (username) {
+    try {
+        return UserModel.findOne({username: username})
+    } catch (error) {
+        throw new Error(`Unable to connect to the database.`)
+    }
+}
+
+/**
+ * Verify if a user already exists with the given email.
+ * @param {String} email 
+ */
+async function findUserByEmail (email) {
+    try {
+        return UserModel.findOne({email: email})
+    } catch (error) {
+        throw new Error(`Unable to connect to the database.`)
+    }
+}
 
 /**
  * GET route for User retrivial.
